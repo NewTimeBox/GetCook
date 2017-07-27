dd544566eb22e094c9ac72c2f48fd0253ded7804
NewTimeBox <ibrahim.hasanov.98@gmail.com>
2017-07-27 19:17:11 +0400

    //

--- app/src/main/java/com/newtimebox/getcook/activities/LoginActivity.java
+++ app/src/main/java/com/newtimebox/getcook/activities/LoginActivity.java
@@ -2,8 +2,10 @@ package com.newtimebox.getcook.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.newtimebox.getcook.R;
+import com.newtimebox.getcook.helpers.General_function;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //ibo commit
+        ImageView backbig = (ImageView) findViewById(R.id.logBackgroudImage);
+        backbig.setImageBitmap(General_function.getBitmap(LoginActivity.this,R.drawable.backbig));
     }
 }
