93f4e58d9587f2f8fc699e88ada163598ae66c17
NewTimeBox <ibrahim.hasanov.98@gmail.com>
2017-08-03 17:58:51 +0400

    //

--- app/src/main/java/com/newtimebox/getcook/activities/LoginActivity.java
+++ app/src/main/java/com/newtimebox/getcook/activities/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.newtimebox.getcook.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 import com.newtimebox.getcook.R;
+import com.newtimebox.getcook.helpers.General_function;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,6 +17,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initialize();
+
         //ibo commit
     }
+
+    private void initialize() {
+        ConstraintLayout mainlayout = (ConstraintLayout) findViewById(R.id.mainlayout);
+
+
+
+
+        Bitmap bmp = General_function.getBitmap(LoginActivity.this,R.drawable.login_background);
+        Drawable d = new BitmapDrawable(getResources(), bmp);
+        mainlayout.setBackground(d);
+    }
 }
+
