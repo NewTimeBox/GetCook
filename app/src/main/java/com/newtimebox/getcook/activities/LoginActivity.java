2380fbd790db5fe9e32b734323ac50c5371ba7ef
NewTimeBox <ibrahim.hasanov.98@gmail.com>
2017-08-03 21:41:57 +0400

    //

--- app/src/main/java/com/newtimebox/getcook/activities/LoginActivity.java
+++ app/src/main/java/com/newtimebox/getcook/activities/LoginActivity.java
@@ -1,20 +1,26 @@
 package com.newtimebox.getcook.activities;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 
 import com.newtimebox.getcook.R;
 import com.newtimebox.getcook.helpers.General_function;
 
 public class LoginActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //ibo commit
-        ImageView backbig = (ImageView) findViewById(R.id.logBackgroudImage);
-        backbig.setImageBitmap(General_function.getBitmap(LoginActivity.this,R.drawable.backbig));
+       // ImageView backbig = (ImageView) findViewById(R.id.logBackgroudImage);
+        ScrollView backbig = (ScrollView) findViewById(R.id.mainScroll);
+        backbig.setBackground(new BitmapDrawable(getResources(), General_function.getBitmap(LoginActivity.this,R.drawable.registerback)));
     }
 }
