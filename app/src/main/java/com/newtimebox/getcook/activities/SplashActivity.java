bda9e5d2a104067eec22a298b03167e4a1317f62
Elkhan <ismayilzadeelxan@gmail.com>
2017-08-12 13:08:41 +0400

    designing

--- app/src/main/java/com/newtimebox/getcook/activities/SplashActivity.java
+++ app/src/main/java/com/newtimebox/getcook/activities/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.newtimebox.getcook.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 import com.newtimebox.getcook.R;
 import com.newtimebox.getcook.helpers.General_function;
@@ -12,6 +16,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final Animation animation = new AlphaAnimation(1, 0);
+        animation.setDuration(500);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(Animation.REVERSE);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView4);
+        imageView.startAnimation(animation);
         General_function.setUpActivity(SplashActivity.this);
         Thread timer = new Thread(){
             public void run(){
